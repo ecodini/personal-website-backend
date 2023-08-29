1dac741099dfe4e320e0aafac6662b64015243e4
ecodini <ecodini1234@gmail.com>
2023-08-29 13:05:04 -0300

    fix some stuff

--- src/Entity/Routes.php
+++ src/Entity/Routes.php
@@ -1,12 +1,10 @@
 <?php namespace Holamanola45\Www\Entity;
 
-use Holamanola45\Www\Entity\Test\TestRoutes;
 use Holamanola45\Www\Entity\User\UserRoutes;
 use Holamanola45\Www\Lib\Http\Router;
 
 class Routes {
     public static function addRoutes(Router $router) {
-        TestRoutes::addRoutes($router);
         UserRoutes::addRoutes($router);
     }
 }
